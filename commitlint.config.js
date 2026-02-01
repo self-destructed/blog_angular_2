929c5f3b2c752883e54b8d7330d65bfd93a898c2
dan <self.destructed.dan@gmail.com>
2026-02-02 02:25:12 +0300

    chore: add husky + commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+
+  rules: {
+    'subject-max-length': [2, 'always', 100],
+    'body-max-line-length': [2, 'always', 100],
+    'footer-max-line-length': [2, 'always', 100]
+  }
+}
