852476a2b80f79e10e61ed950667ef15eff1e385
dan <self.destructed.dan@gmail.com>
2026-02-04 21:12:54 +0300

    feat(app): add home, not-found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router'
+import { Home } from './pages/home/home'
 
-export const routes: Routes = []
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+    title: 'Home'
+  },
+
+  {
+    path: 'not-found',
+    loadComponent: () =>
+      import('./pages/not-found/not-found').then((m) => m.NotFound),
+    title: 'Not found'
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: 'not-found'
+  }
+]
