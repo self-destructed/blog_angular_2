6e64c004fc663a48b1b948c826dedb812521d954
dan <self.destructed.dan@gmail.com>
2026-02-16 13:13:54 +0300

    feat(app): add header to app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core'
 import { RouterLink, RouterOutlet } from '@angular/router'
+import { HeaderComponent } from './shared/header/header'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, HeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
