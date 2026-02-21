918e6ec4dfa6682825d145ee3984582d5c813487
dan <self.destructed.dan@gmail.com>
2026-02-21 14:50:32 +0300

    chore: add primeng

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,9 +3,21 @@ import {
   provideBrowserGlobalErrorListeners
 } from '@angular/core'
 import { provideRouter } from '@angular/router'
-
+import { providePrimeNG } from 'primeng/config'
+import Aura from '@primeuix/themes/aura'
 import { routes } from './app.routes'
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)]
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: false
+        }
+      }
+    })
+  ]
 }
