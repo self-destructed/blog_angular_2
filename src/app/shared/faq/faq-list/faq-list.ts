d4907d1b79091f4fe9ae5cbf124575b96d2972c8
dan <self.destructed.dan@gmail.com>
2026-03-01 17:57:26 +0300

    refactor(faq-list): replace custom FaqListComponent to primeng accordion

--- src/app/shared/faq/faq-list/faq-list.ts
+++ src/app/shared/faq/faq-list/faq-list.ts
@@ -1,10 +1,11 @@
 import { Component, input } from '@angular/core'
 import { FaqItem } from '../../../core/types/faq-item.type'
 import { FaqItemComponent } from '../faq-item/faq-item'
+import { AccordionModule } from 'primeng/accordion'
 
 @Component({
   selector: 'app-faq-list',
-  imports: [FaqItemComponent],
+  imports: [FaqItemComponent, AccordionModule],
   templateUrl: './faq-list.html',
   styles: ``
 })
