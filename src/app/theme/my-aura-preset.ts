a55dd458c7e4018b87236406f0b5df17eb6f8519
dan <self.destructed.dan@gmail.com>
2026-03-03 16:58:17 +0300

    refactor(app): replace object colors with pallete function

--- src/app/theme/my-aura-preset.ts
+++ src/app/theme/my-aura-preset.ts
@@ -1,23 +1,9 @@
-import { definePreset } from '@primeuix/themes'
+import { definePreset, palette } from '@primeuix/themes'
 import Aura from '@primeuix/themes/aura'
-
-const persianGreen = {
-  50: '#e6fffa',
-  100: '#bffaf0',
-  200: '#8ff2e3',
-  300: '#5be8d4',
-  400: '#2cdcc2',
-  500: '#00a693', // persian-green
-  600: '#008b7a',
-  700: '#007062',
-  800: '#00584c',
-  900: '#004238',
-  950: '#002a24'
-}
 
 export const MyAuraPreset = definePreset(Aura, {
   semantic: {
-    primary: persianGreen,
+    primary: palette('#00aaa1'),
     colorScheme: {
       light: {
         primary: {
